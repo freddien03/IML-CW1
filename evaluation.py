8f3cf60c2ebd63291f71be176807c274d53cf346
bsychen <85930791+bsychen@users.noreply.github.com>
2025-10-27 20:59:25 +0000

    more functions for eval

--- evaluation.py
+++ evaluation.py
@@ -2,13 +2,30 @@ import numpy as np
 
 from node import Node
 
+def predict_one(tree, x):
+    while isinstance(tree, Node):
+        tree = tree.right if x[tree.attr] > tree.val else tree.left
+    return tree.label
+
+def predict(tree, X):
+    return np.array([predict_one(tree, x) for x in X])
+
 def evaluate(test_db, trained_tree):
     X, y = test_db[:, :-1], test_db[:, -1]
-
-    def _predict_one(x, t):
-        while isinstance(t, Node):
-            t = t.right if x[t.attr] > t.val else t.left
-        return t.label
-
-    y_hat = np.array([_predict_one(x, trained_tree) for x in X])
+    y_hat = predict(trained_tree, X)
+    
     return float(np.mean(y_hat == y))
+
+def confusion_matrix(y_true, y_pred, labels=None):
+    if labels is None:
+        labels = np.unique(np.concatenate([y_true, y_pred]))
+    
+    L = len(labels)
+    idx = {lab: i for i, lab in enumerate(labels)}
+    cm = np.zeros((L, L), dtype=float)
+    
+    for t, p in zip(y_true, y_pred):
+        if t in idx and p in idx:
+            cm[idx[t], idx[p]] += 1
+    
+    return cm, np.array(labels)
