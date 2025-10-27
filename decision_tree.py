392a51b689c9671e2e3cf02b9fd2afe4a667db1b
freddien03 <60051731+freddien03@users.noreply.github.com>
2025-10-27 20:43:17 +0000

    completed decision tree

--- decision_tree.py
+++ decision_tree.py
@@ -4,21 +4,81 @@ from node import Node, Leaf
 clean = np.loadtxt("wifi_db/clean_dataset.txt")
 noisy = np.loadtxt("wifi_db/noisy_dataset.txt")
 
-class Decision_Tree:
+class Decision_Tree_Classifier:
 
-    def __init__(self):
-        pass
+    def __init__(self, data=None):
+        self.data = data
+        if data is not None:
+            self.tree, self.depth = self.decision_tree_learning(data, 0)
+        self.tree = None
+
+    def predict(self, data, tree):
+        while type(tree) is Node:
+            if data[tree.attr] > tree.val:
+                tree = tree.right
+            else:
+                tree = tree.left
+        
+        return tree.label
+
         
     def decision_tree_learning(self, data: np.array, depth: int):
-        if np.all(data[:,-1] == data[:,-1][0]):
+        if len(np.unique(data[:,-1])) == 1:
             return (Leaf(data[:,-1][0]), depth)
         
-        attr, val, l_data, r_data = self.find_split()
+        attr, val, l_data, r_data = self.find_split(data)
         l_branch, l_depth = self.decision_tree_learning(l_data, depth+1)
         r_branch, r_depth = self.decision_tree_learning(r_data, depth+1)
+
         return (Node(attr, val, l_branch, r_branch), max(l_depth, r_depth))
 
     
 
     def find_split(self, data):
-        return
+        attr = None
+        val = None
+        l_data = None
+        r_data = None
+        min_rem = np.inf
+        for j in range(data.shape[1] - 1):
+            sorted_data = data[data[:,j].argsort()]
+
+            i = 1
+            while i < len(sorted_data):
+                old = sorted_data[i-1, j]
+                while i < len(sorted_data) and sorted_data[i, j] == old:
+                    i += 1
+                if i == len(sorted_data):
+                    break
+
+                left = sorted_data[:i]
+                right = sorted_data[i:]
+                rem = self.calc_rem(left, right)
+
+                if rem < min_rem:
+                    attr = j
+                    val = (sorted_data[i - 1, j] + sorted_data[i, j]) / 2
+                    l_data = left
+                    r_data = right
+                    min_rem = rem
+
+                i += 1
+
+        return (attr, val, l_data, r_data)
+        
+
+    
+    def calc_H(self, data):
+        labels = data[:,-1]
+        N = len(labels)
+        total = 0
+        uniques, counts = np.unique(labels, return_counts=True)
+        for l, n in zip(uniques, counts):
+            total += (n / N) * np.log2(n / N)
+        return -total
+    
+    def calc_rem(self, left, right):
+        ln = len(left)
+        rn = len(right)
+
+        return (ln / (ln + rn)) * self.calc_H(left) + (rn / (ln + rn)) * self.calc_H(right)
--- node.py
+++ node.py
@@ -4,7 +4,13 @@ class Node:
         self.val = val
         self.left = left
         self.right = right
+    
+    def __str__(self):
+        return f"({self.left}) <- {self.attr} < {self.val} -> ({self.right})"
 
 class Leaf:
     def __init__(self, label):
         self.label = label
+    
+    def __str__(self):
+        return f"l: {self.label}"
